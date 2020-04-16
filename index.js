28745f306d210e020bcc52e9106e0ae5a91b7243
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-04-16 19:21:00 -0400

    logger middleware created

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server= require('./server.js');
 const port= 3005;
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}...`);
+    console.log(`\n Server listening on port ${port}... \n`);
 });
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const helmet= require('helmet')
+const posts_router= require('./posts/postRouter.js');
+const users_router= require('./users/userRouter.js');
+const logger= require('./custom_middleware/logger.js');
+const welcome_router= require('./welcome/welcome-router.js');
 
 const server = express();
 
 // built-in middleware
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.send(`<h4>Let's write some middleware!</h4>`);
+//third party middleware
+server.use(helmet());
+
+// custom middleware
+server.use(logger)
+
+// route handlers
+server.use(welcome_router);
+
+server.use((req, res) => {
+	res.status(404).send(
+      `<h4 align='center'>The url ${req.url.toUpperCase()} was not found.</h4>`
+    )
 });
-
-//custom middleware
-
-function logger(req, res, next) {}
 
 module.exports = server;
