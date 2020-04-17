d781f67d6851b3ba9a4762ad344a638d67ac51c9
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-04-16 21:39:22 -0400

    post method successful

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,14 +3,23 @@ const db= require('./userDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+// POST /users (create user)
+router.post('/', validateUser(), async (req, res) => {
+  try{
+    const createUser= await db.insert(req.body)
+    res.status(201).json(createUser);
+  } catch(err){
+    console.log('Error posting:', err);
+    next(err);
+  }
 });
 
+// POST /users/:id/posts (create post by user)
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+// GET /users (all users)
 router.get('/', async (req, res) => {
   try{
     const usersData= await db.get()
@@ -20,24 +29,29 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET /users/:id (users by id)
 router.get('/:id', validateUserId(), (req, res) => {
   res.json(req.user);
 });
 
+// GET /users/:id/posts (posts by user)
 router.get('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+// DELETE /users/:id (delete user)
 router.delete('/:id', (req, res) => {
   // do your magic!
 });
 
+// PUT /users/:id (update a user)
 router.put('/:id', (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
+// validating a user by its id
 function validateUserId() {
   return async (req, res, next) =>{
     try{
@@ -47,7 +61,7 @@ function validateUserId() {
         next();
       } else{
         res.status(404).json({
-          error_message: 'Invalid user id.'
+          request_errorMessage: 'Invalid user id.'
         })
       }
     } catch(err){
@@ -57,10 +71,17 @@ function validateUserId() {
   }
 }
 
-
-
-function validateUser(req, res, next) {
-  // do your magic!
+// validating if the user properly sent name data or none at all
+function validateUser() {
+  return (req, res, next) =>{
+    if(!req.body.name){
+      return res.status(400).json({
+        error_message: 'Missing user data.',
+      })
+    } else{
+      next();
+    }
+  } 
 }
 
 function validatePost(req, res, next) {
